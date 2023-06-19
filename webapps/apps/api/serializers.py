b5d0d2e76529c9c239da725fa80d218c35be9e59
anderson <andersonsg@gmail.com>
2023-06-19 15:42:14 +0000

    ajuste cacched property

--- webapps/apps/api/serializers.py
+++ webapps/apps/api/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers  # Importa a classe serializers do módulo rest_framework
+from django.utils.functional import cached_property
 from django.apps import apps  # Importa o módulo apps do Django
 Proposta = apps.get_model('emprestimo', 'Proposta')  # Obtém o modelo Proposta do aplicativo 'emprestimo'
 
@@ -26,7 +27,6 @@ class PropostaAdminSerializer(serializers.ModelSerializer):
         fields = '__all__'  # Define todos os campos do modelo a serem incluídos na serialização
 
 class PropostaSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Proposta  # Define o modelo a ser serializado
         fields = ['id', 'nome_completo', 'cpf', 'endereco', 'valor']  # Define os campos do modelo a serem incluídos na serialização
